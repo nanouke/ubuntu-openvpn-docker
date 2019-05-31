5336377ace21296e344bbc60630b9c7bdc4079e9
Philippe Vadnais <philippeg.vadnais@hotmail.com>
2019-05-30 17:33:39 -0700

    remove ts commande need to install more utils no need

--- ressources/install.sh
+++ ressources/install.sh
@@ -15,9 +15,9 @@ VPN_CONFIG=$(find /config/openvpn -maxdepth 1 -name "*.ovpn" -print -quit)
 
 # if ovpn file not found in /config/openvpn then exit
 if [[ -z "${VPN_CONFIG}" ]]; then
-	echo "[crit] No OpenVPN config file located in /config/openvpn/ (ovpn extension), please download from your VPN provider and then restart this container, exiting..." | ts '%Y-%m-%d %H:%M:%.S' && exit 1
+	echo "[crit] No OpenVPN config file located in /config/openvpn/ (ovpn extension), please download from your VPN provider and then restart this container, exiting..." && exit 1
 fi
 
-echo "[info] OpenVPN config file (ovpn extension) is located at ${VPN_CONFIG}" | ts '%Y-%m-%d %H:%M:%.S'
+echo "[info] OpenVPN config file (ovpn extension) is located at ${VPN_CONFIG}" 
 
 openvpn ${VPN_CONFIG}
